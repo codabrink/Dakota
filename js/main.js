98cc516350cc9d1e76b3ede1d4241a955f50859c
Dakota Brink <codabrink@gmail.com>
2014-07-29 22:34:30 -0400

    I like this

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(function() {
+  cloud_float($('#c1'), 14000);
+  cloud_float($('#c2'), 20000);
+  cloud_float($('#c3'), 26000);
+});
+
+
+function cloud_float(cloud, time) {
+  cloud.animate({
+    left: '-=750px'
+  }, time, 'linear', function() {
+    cloud.css('left', '0');
+    cloud_float(cloud, time);
+  });  
+}
