93ecf9a17e53d70ef477541b1623bd41922a719e
Dakota Brink <codabrink@gmail.com>
2014-07-30 15:12:38 -0400

    fancy

--- js/main.js
+++ js/main.js
@@ -1,23 +1,11 @@
 $(function() {
-  //cloud_float($('#c1'), 14000);
-  //cloud_float($('#c2'), 20000);
-  //cloud_float($('#c3'), 26000);
-
   setTimeout(function() {
     $('.ground-cloud').addClass('low');
     setTimeout(function() {
+      $('.ground-cloud').remove();
       $('.cloud').removeClass('high');
+      $('#ground').removeClass('low');
+      $('#sun').removeClass('low');
     }, 3000);
   }, 3000);
-
 });
-
-
-function cloud_float(cloud, time) {
-  cloud.animate({
-    left: '-=750px'
-  }, time, 'linear', function() {
-    cloud.css('left', '0');
-    cloud_float(cloud, time);
-  });  
-}
